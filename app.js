b8ff2d6a240821aad936791e44c358d86c65b78c
kavellewis <kavellewis@gmail.com>
2016-07-09 12:13:09 -0400

    Committing merge

---
+++ app.js
@@ -0,0 +1,9 @@
+// alert("Hello")
+// var typed_stuff = prompt ("Enter your name")
+// if(typed_stuff == "Mr.Lewis"){
+// alert("That's a dumb name.")
+// } else {
+// alert("that's a nice name")
+// }
+var field = document.getElementById("seach_field")
+field.value = "search Here"
